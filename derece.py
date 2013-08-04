abb7efaa57a665479f5ac66693eab1e9244aa30a
gizem <giskenderoglu@gmail.com>
2013-08-04 17:49:53 +0300

    other python codes

---
+++ derece.py
@@ -0,0 +1,3 @@
+santigirat = input("santigirat cinsinden bir derece girin ")
+fahrenayt = santigirat * (9.0/5.0) + 32
+print ("%s santigirat %s fahrenayt a esittir") %(santigirat, fahrenayt)
---
+++ ders8.py
@@ -0,0 +1,6 @@
+try:
+    sayi = int(raw_input("sayi giriniz:"))
+    print sayi
+except ValueError:
+    print ("lutfen harf degil sayi girin")
+print ("tenks!")
