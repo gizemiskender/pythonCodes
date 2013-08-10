28f2b8db726d9eeec15ab5693de778f2e0fe74c0
gizem <giskenderoglu@gmail.com>
2013-08-10 11:18:20 +0300

    ders10 eklendi

---
+++ ders10.py
@@ -0,0 +1,18 @@
+# demetler (tuple)
+
+demet = "gizem", 21
+print type(demet)
+
+dersler = "mat", "fizik", "kimya" # packing (demetleme)
+a, b, c = dersler # unpacking    demet cozme
+print a
+
+print dir(tuple)
+
+demet = "python", "ruby", "C", "C++"
+print demet.index("C") # demet icindeki yerini buldum.
+print demet.count("C") # demet icinde kac defa gectigini buldum.
+
+
+
+
